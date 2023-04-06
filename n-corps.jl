e13e28381a64feece16f12a10121738ab252eb55
MedButch <C3r3b0s666>
2023-04-06 15:46:53 -0400

    Predictor avec k calculé pour le système

--- n-corps.jl
+++ n-corps.jl
@@ -1,4 +1,4 @@
-#!/bin/env julia
+#!/home/medbutch/julia-1.8.1/bin/julia julia
 
 using StaticArrays #https://juliaarrays.github.io/StaticArrays.jl/stable/pages/api/
 using Plots
@@ -18,30 +18,35 @@ struct Body <: FieldVector{5, Float64}
 end
 
 # f also has 4 parts
-function f(x::Body)
+function f(x::Body, k::Float64)
     x̃r = x.p/x.m #x.vr = x.p/x.m
     x̃θ = x.l/(x.r^2*x.m)
-    x̃p = x.l^2/(x.r^3*x.m) - 1.5/x.r^2
+    x̃p = x.l^2/(x.r^3*x.m) - k/x.r^2
     return Body(x̃r, x̃θ, x̃p, 0, 0)
 end
 
 # x̃ = x_o + τ * f(x_0, t)
-function predictorStep(x_0::Body, τ::Float64)
-    return x_0 + τ * f(x_0)
+function predictorStep(x_0::Body, τ::Float64, k::Float64)
+    return x_0 + τ * f(x_0, k)
 end
 
-
-
 function test(n::Int64)
-    x = [Body(1, 0, 0, 1,1)]
-    for t ∈ 2:n
-        push!(x, predictorStep(x[t-1], 0.01))
+    x = [Body(1, 0, 0, 1, 1)] #Test body timestep 0.01s
+    # x = [Body(362600000, 3.14, 1022,  2.9e34, 7.342e22)] #Lune, timestep 2551.4428896s
+    # x = [Body(408000, 0, 7660, 3125280000, 450000)] #ISS timestep 5.574s
+    # m = 5.972e24 #Masse Centrale Terre
+    m = 2.2475277195085407e10 #Test central mass
+    G = 6.674e-11
+    k = G * m * x[1].m
+    for t in 2:n
+        push!(x, predictorStep(x[t-1], 0.01, k))
     end
     return x
 end
 
-test(2)
+# test(2)
 bs = test(1000)
+# print(bs)
 
 @userplot CirclePlot
 @recipe function f(cp::CirclePlot)
@@ -63,6 +68,3 @@ anim = @animate for i ∈ 1:1000
     circleplot(x, y, i)
 end
 gif(anim, "anim_fps60.gif", fps = 60)
-
-
-
